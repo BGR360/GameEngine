d3ba92b4cb0d9a7796ab5195c6b8477b20a97f35
Ben <benwolverine2019@gmail.com>
2015-08-26 12:53:54 -0500

    Finished stubbing out Vector2fTest

--- src/test/com/bengine/math/Vector2fTest.java
+++ src/test/com/bengine/math/Vector2fTest.java
@@ -29,61 +29,105 @@ public class Vector2fTest extends TestCase
 
     public void testNormalize() throws Exception
     {
-
-    }
-
-    public void testNormalized() throws Exception
-    {
-
+        Vector2f vec = new Vector2f(5, 5);
+        Vector2f expected = new Vector2f((float)Math.sqrt(2.0f) / 2.0f, (float)Math.sqrt(2.0f) / 2.0f);
+        Vector2f result = vec.normalized();
+        vec.normalize();
+        assertEquals(expected, result);
+        assertEquals(result, vec);
     }
 
     public void testAdd() throws Exception
     {
-
-    }
-
-    public void testAdd1() throws Exception
-    {
-
+        Vector2f left = new Vector2f(1.0f, -3.14f);
+        Vector2f right = new Vector2f(0.0f, 42.0f);
+        Vector2f expected = new Vector2f(1.0f, 38.86f);
+        Vector2f result = left.add(right);
+        assertEquals(expected, result);
     }
 
     public void testSub() throws Exception
     {
-
-    }
-
-    public void testSub1() throws Exception
-    {
-
+        Vector2f left = new Vector2f(1.0f, -3.14f);
+        Vector2f right = new Vector2f(0.0f, 42.0f);
+        Vector2f expected = new Vector2f(1.0f, -45.14f);
+        Vector2f result = left.sub(right);
+        assertEquals(expected, result);
     }
 
     public void testMul() throws Exception
     {
-
+        Vector2f left = new Vector2f(1.0f, -3.14f);
+        Vector2f right = new Vector2f(0.0f, 2.0f);
+        Vector2f expected = new Vector2f(0.0f, -6.28f);
+        Vector2f result = left.mul(right);
+        assertEquals(expected, result);
     }
 
-    public void testMul1() throws Exception
+    public void testScalarMul() throws Exception
     {
-
+        Vector2f left = new Vector2f(1.0f, -3.14f);
+        float right = 2.0f;
+        Vector2f expected = new Vector2f(2.0f, -6.28f);
+        Vector2f result = left.mul(right);
+        assertEquals(expected, result);
     }
 
     public void testDiv() throws Exception
     {
-
+        Vector2f left = new Vector2f(1.0f, -3.14f);
+        Vector2f right = new Vector2f(0.0f, 2.0f);
+        Vector2f expected = new Vector2f(0.0f, -6.28f);
+        Vector2f result = left.div(right);
+        assertEquals(expected, result);
     }
 
-    public void testDiv1() throws Exception
+    public void testScalarDiv() throws Exception
     {
-
+        Vector2f left = new Vector2f(1.0f, -3.14f);
+        float right = 2.0f;
+        Vector2f expected = new Vector2f(0.5f, -1.57f);
+        Vector2f result = left.div(right);
+        assertEquals(expected, result);
     }
 
     public void testEquals() throws Exception
     {
-
+        Vector2f vec1 = new Vector2f();
+        Vector2f vec2 = new Vector2f();
+        boolean equals = vec1.equals(vec2);
+        assertTrue(equals);
     }
 
     public void testCompareTo() throws Exception
     {
+        Vector2f vec1 = new Vector2f(-1.0f, 0.0f);
+        Vector2f vec2 = new Vector2f(-1.0f, 5.0f);
+        Vector2f vec3 = new Vector2f(2.0f, -3.2f);
+
+        int comp1_1 = vec1.compareTo(vec1);
+        int comp1_2 = vec1.compareTo(vec2);
+        int comp1_3 = vec1.compareTo(vec3);
+
+        int comp2_1 = vec2.compareTo(vec1);
+        int comp2_2 = vec2.compareTo(vec2);
+        int comp2_3 = vec2.compareTo(vec3);
+
+        int comp3_1 = vec3.compareTo(vec1);
+        int comp3_2 = vec3.compareTo(vec2);
+        int comp3_3 = vec3.compareTo(vec3);
+
+        assertTrue(comp1_1 == 0);
+        assertTrue(comp1_2 < 0);
+        assertTrue(comp1_3 < 0);
+
+        assertTrue(comp2_1 > 0);
+        assertTrue(comp2_2 == 0);
+        assertTrue(comp2_3 < 0);
+
+        assertTrue(comp3_1 > 0);
+        assertTrue(comp3_2 > 0);
+        assertTrue(comp3_3 == 0);
 
     }
 }
